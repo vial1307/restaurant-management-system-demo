7f932891fe32b24c29ca3ece8149c3dfea8a46b8
vial1307 <technical.tts@gmail.com>
2026-08-31 20:39:13 +0800

    Deploy large freezer inventory update

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "shitu-kitchen-os-v10";
+const CACHE_NAME = "shitu-kitchen-os-v11";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./manifest.webmanifest?v=10",
-  "./src/app.js?v=10",
+  "./manifest.webmanifest?v=11",
+  "./src/app.js?v=11",
   "./src/store.js",
   "./src/rules.js",
   "./src/i18n.js",
@@ -11,7 +11,7 @@ const APP_SHELL = [
   "./src/management.js",
   "./src/skills.js",
   "./src/qr.js",
-  "./src/styles.css?v=10",
+  "./src/styles.css?v=11",
   "./src/icon.svg",
 ];
 
