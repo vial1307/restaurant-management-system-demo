cb40d18c1654450d89b68abb4d6eefdb1faefb31
vial1307 <technical.tts@gmail.com>
2026-09-01 20:53:47 +0800

    Release visible branch inventory edit controls v54

--- src/app.js
+++ src/app.js
@@ -1759,7 +1759,7 @@ if (globalThis.navigator?.serviceWorker && window.location.protocol !== "file:")
   });
 
   globalThis.navigator.serviceWorker
-    .register("./sw.js?v=53", { updateViaCache: "none" })
+    .register("./sw.js?v=54", { updateViaCache: "none" })
     .then(async (registration) => {
       await registration.update().catch(() => {});
       if (registration.waiting) registration.waiting.postMessage({ type: "SKIP_WAITING" });
