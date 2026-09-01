7b49298d88affd2079c518ebbc2c4173e98b5ff5
vial1307 <technical.tts@gmail.com>
2026-09-01 13:23:31 +0800

    chore: register service worker v40

--- src/app.js
+++ src/app.js
@@ -1445,7 +1445,7 @@ if (globalThis.navigator?.serviceWorker && window.location.protocol !== "file:")
   });
 
   globalThis.navigator.serviceWorker
-    .register("./sw.js?v=39", { updateViaCache: "none" })
+    .register("./sw.js?v=40", { updateViaCache: "none" })
     .then(async (registration) => {
       await registration.update().catch(() => {});
       if (registration.waiting) registration.waiting.postMessage({ type: "SKIP_WAITING" });
