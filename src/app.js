8f8f26555756f0dbd7ce1c3f94d007471670d643
vial1307 <technical.tts@gmail.com>
2026-09-01 20:26:07 +0800

    Release branch manager auto-routing inventory update

--- src/app.js
+++ src/app.js
@@ -1740,7 +1740,7 @@ if (globalThis.navigator?.serviceWorker && window.location.protocol !== "file:")
   });
 
   globalThis.navigator.serviceWorker
-    .register("./sw.js?v=50", { updateViaCache: "none" })
+    .register("./sw.js?v=51", { updateViaCache: "none" })
     .then(async (registration) => {
       await registration.update().catch(() => {});
       if (registration.waiting) registration.waiting.postMessage({ type: "SKIP_WAITING" });
