9bb0beac5ddd4621a2e79329a1cf49318b37bf13
vial1307 <technical.tts@gmail.com>
2026-09-01 18:35:52 +0800

    Release inventory management form update

--- src/app.js
+++ src/app.js
@@ -1604,7 +1604,7 @@ if (globalThis.navigator?.serviceWorker && window.location.protocol !== "file:")
   });
 
   globalThis.navigator.serviceWorker
-    .register("./sw.js?v=44", { updateViaCache: "none" })
+    .register("./sw.js?v=45", { updateViaCache: "none" })
     .then(async (registration) => {
       await registration.update().catch(() => {});
       if (registration.waiting) registration.waiting.postMessage({ type: "SKIP_WAITING" });
