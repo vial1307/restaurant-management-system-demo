96783f825ac4e4a03a55f18b1ef65aed9521b1ff
vial1307 <technical.tts@gmail.com>
2026-09-01 19:48:28 +0800

    Release linked inventory function sync

--- src/app.js
+++ src/app.js
@@ -1685,7 +1685,7 @@ if (globalThis.navigator?.serviceWorker && window.location.protocol !== "file:")
   });
 
   globalThis.navigator.serviceWorker
-    .register("./sw.js?v=48", { updateViaCache: "none" })
+    .register("./sw.js?v=49", { updateViaCache: "none" })
     .then(async (registration) => {
       await registration.update().catch(() => {});
       if (registration.waiting) registration.waiting.postMessage({ type: "SKIP_WAITING" });
