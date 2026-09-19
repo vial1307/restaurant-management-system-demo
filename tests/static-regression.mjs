cb98749f7b5de9469c4bc952a0283805b586540e
vial1307 <technical.tts@gmail.com>
2026-09-19 14:54:39 +0800

    test: include inventory site isolation contract

--- tests/static-regression.mjs
+++ tests/static-regression.mjs
@@ -14,6 +14,7 @@ import {
 import { searchMatches } from "../src/search-utils.js";
 import "./system-port-regression.mjs";
 import "./inventory-master-data-regression.mjs";
+import "./inventory-site-isolation-contract-regression.mjs";
 
 const ROOT = path.resolve(new URL("..", import.meta.url).pathname);
 const read = (file) => fs.readFileSync(path.join(ROOT, file), "utf8");
